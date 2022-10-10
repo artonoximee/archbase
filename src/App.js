13bec7a15f86dfcf3cd6ec469809f86589741191
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-10 15:17:25 +0200

    minimal code added for navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div id="page" className="container-fluid bg-dark text-light">
-      <div className="container bg-dark text-light">
-        <h1>Hello world</h1>
+      <div className="container">
+        
+        <div class="row justify-content-center">
+          <div class="col-lg-8">
+            <Navbar />
+            <h1>Hello world</h1>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
