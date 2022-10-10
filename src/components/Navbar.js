049d630de03b4faaad69a6250d8dfe814dc819ea
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-10 15:22:50 +0200

    updated Navbar content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,34 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar w/ text</a>
+        <Link to="/" className="navbar-brand"><i className="fa-solid fa-server text-primary"></i> Archbase</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <Link to="/" className="nav-link">Accueil</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
+              <Link to="/" className="nav-link">Tarifs</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
+              <Link to="/" className="nav-link">À Propos</Link>
             </li>
           </ul>
-          <span className="navbar-text">
-            Navbar text with an inline element
-          </span>
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Inscription</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Connexion</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
