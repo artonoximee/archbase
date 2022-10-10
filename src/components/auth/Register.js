7cf706f3ba8d79da7b4b5a9027796a2d42f2c6ae
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-10 18:14:10 +0200

    finished styling login and signup forms

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -24,22 +24,28 @@ function SignUp() {
         <div className="col-lg-6 col-md-12">
 
           <h3>Inscription</h3>
-
+          
+          <label htmlFor="email" className="form-label mt-5"><i className="fa-solid fa-envelope text-primary"></i> Email</label>
           <input 
             type="email"
-            className={`form-control form-control-lg mt-3 bg-dark border-secondary text-light ${errors.email && "is-invalid border-danger"}`}
+            id="email"
+            className={`form-control form-control-lg bg-dark border-secondary text-light ${errors.email && "is-invalid border-danger"}`}
             placeholder="Adresse email"
             {...register("email", { required: true, pattern: /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i })}
           />
-          {errors.email && <p className="text-danger mt-2">Merci de renseigner une adresse email valide pour vous inscrire</p>}
+          {errors.email && <div className="form-text text-danger">Merci de renseigner une adresse email valide pour vous inscrire</div>}
 
+          <label htmlFor="password" className="form-label mt-3"><i className="fa-sharp fa-solid fa-key text-primary"></i> Mot de passe</label>
           <input 
             type="password"
-            className={`form-control form-control-lg mt-3 bg-dark border-secondary text-light ${errors.password && "is-invalid border-danger"}`}
+            id="password"
+            className={`form-control form-control-lg bg-dark border-secondary text-light ${errors.password && "is-invalid border-danger"}`}
             placeholder="Mot de passe"
             {...register("password", { required: true, minLength: 6 })}
           />
-          {errors.password && <p className="text-danger mt-2">Merci de renseigner un mot de passe de 6 caractères minimum pour vous inscrire</p>}
+          <div class={`form-text ${errors.password && "text-danger"}`}>
+            Votre mot de passe doit au minimum contenir 6 caractères.
+          </div>
 
           <div className="d-grid gap-2">
             <button className={`btn btn-lg mt-5 btn-outline-primary`} onClick={handleSubmit(signUp)} type="submit">Inscription</button>
