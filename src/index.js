a7858ec0d32d316c790c269044006c3408d9279f
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-10 14:36:13 +0200

    installed jquery and correct importing in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
