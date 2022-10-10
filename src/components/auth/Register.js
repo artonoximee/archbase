bb7cb08bf43eedf68031244f9e5534b3a76d0ad0
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-10 17:46:34 +0200

    added validation on register form

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,15 +1,15 @@
 import React, {useState} from "react";
+import {useForm} from "react-hook-form";
 import {createUserWithEmailAndPassword} from "firebase/auth";
 import {auth} from '../../firebase-config'
 
 function SignUp() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  // const [errorLogin, setErrorLogin] = useState(false);
 
-  async function register() {
+  async function signUp(data) {
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
+      await createUserWithEmailAndPassword(auth, data.email, data.password);
       window.location.reload();
     } catch(err) {
       console.log(err.message)
@@ -24,25 +24,26 @@ function SignUp() {
             type="text"
             className={`form-control form-control-lg mt-3 bg-dark border-secondary text-light`}
             placeholder="Nom et prénom"
-            onChange={e => setName(e.target.value)}
           />
 
           <input 
             type="email"
-            className={`form-control form-control-lg mt-3 bg-dark border-secondary text-light`}
+            className={`form-control form-control-lg mt-3 bg-dark border-secondary text-light ${errors.email && "is-invalid border-danger"}`}
             placeholder="Adresse email"
-            onChange={e => setEmail(e.target.value)}
+            {...register("email", { required: true, pattern: /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i })}
           />
+          {errors.email && <p className="text-danger mt-2">Merci de renseigner une adresse email valide pour vous inscrire</p>}
 
           <input 
             type="password"
-            className={`form-control form-control-lg mt-3 bg-dark border-secondary text-light`}
+            className={`form-control form-control-lg mt-3 bg-dark border-secondary text-light ${errors.password && "is-invalid border-danger"}`}
             placeholder="Mot de passe"
-            onChange={e => setPassword(e.target.value)}
+            {...register("password", { required: true, minLength: 6 })}
           />
+          {errors.password && <p className="text-danger text-center mt-2">Merci de renseigner un mot de passe de 6 caractères minimum pour vous inscrire</p>}
 
           <div className="d-grid gap-2">
-            <button className={`btn btn-lg mt-5 btn-outline-primary`} onClick={register} type="submit">Inscription</button>
+            <button className={`btn btn-lg mt-5 btn-outline-primary`} onClick={handleSubmit(signUp)} type="submit">Inscription</button>
           </div>
 
           <div className=" bottom-margin"></div>
