d6aee79211655e8d91032b8992272136b6830ba9
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-10 15:39:09 +0200

    sign in form is done

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -2,7 +2,28 @@ import React from "react";
 
 function SignIn() {
   return (
-    <h1>Connexion</h1>
+    <div className="row justify-content-center top-margin">
+        <div className="col-lg-6 col-md-12">
+
+          <input 
+            type="email"
+            className={`form-control form-control-lg mt-3 bg-dark border-secondary text-light`}
+            placeholder="Adresse email"
+          />
+
+          <input 
+            type="password"
+            className={`form-control form-control-lg mt-3 bg-dark border-secondary text-light`}
+            placeholder="Mot de passe"
+          />
+
+          <div className="d-grid gap-2">
+            <button className={`btn btn-lg mt-5 btn-outline-primary`} onClick={null} type="submit">Connexion</button>
+          </div>
+
+          <div className=" bottom-margin"></div>
+        </div>
+      </div>
   )
 }
 export default SignIn;
