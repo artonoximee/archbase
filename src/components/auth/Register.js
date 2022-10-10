9fbee155927249290de9bfa98854b0d4999eaa47
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-10 16:01:12 +0200

    useStates in register form - done

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 
 function SignUp() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <div className="row justify-content-center top-margin">
         <div className="col-lg-6 col-md-12">
@@ -9,18 +13,21 @@ function SignUp() {
             type="text"
             className={`form-control form-control-lg mt-3 bg-dark border-secondary text-light`}
             placeholder="Nom et prénom"
+            onChange={e => setName(e.target.value)}
           />
 
           <input 
             type="email"
             className={`form-control form-control-lg mt-3 bg-dark border-secondary text-light`}
             placeholder="Adresse email"
+            onChange={e => setEmail(e.target.value)}
           />
 
           <input 
             type="password"
             className={`form-control form-control-lg mt-3 bg-dark border-secondary text-light`}
             placeholder="Mot de passe"
+            onChange={e => setPassword(e.target.value)}
           />
 
           <div className="d-grid gap-2">
