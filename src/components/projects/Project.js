f1a3b650d5c1d3a422c1efb2d37b1a9b1ec2d56c
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-11 15:17:10 +0200

    correctly displaying project page

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,10 +1,37 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { db } from "../../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 function Project() {
   const { projectId } = useParams();
+  const [project, setProject] = useState();
+  
+  useEffect(() => {
+    async function getProject() {
+      const docRef = doc(db, "projects", projectId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setProject(docSnap.data());
+      }
+    }
+    getProject()
+  }, [])
+
   return (
-    <h3 className="top-margin">Projet {projectId}</h3>
+    <>
+      {
+        project &&
+        <div className="row top-margin">
+          <div className="col">
+            <Link to="/dashboard" className="btn btn-sm btn-outline-primary"><i className="fa-solid fa-left-long"></i> Retour</Link>
+            <h3 className="mt-4">{project.projectName}</h3>
+          </div>
+        </div>
+        
+      }
+    </>
+    
   )
 }
 
