c233a53f760fcb65a5664d5307b748ac6ca1dd54
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-11 11:19:30 +0200

    routes are working and navbar is correctly directing to components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
       <div className="container-fluid">
-        <a href="/" className="navbar-brand"><i className="fa-solid fa-server text-primary"></i> Archbase</a>
+        <Link to="/" className="navbar-brand"><i className="fa-solid fa-server text-primary"></i> Archbase</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a href="/" className="nav-link">Accueil</a>
+              <Link to="/" className="nav-link">Accueil</Link>
             </li>
             <li className="nav-item">
-              <a href="/" className="nav-link">Tarifs</a>
+              <Link to="/" className="nav-link">Tarifs</Link>
             </li>
             <li className="nav-item">
-              <a href="/" className="nav-link">À Propos</a>
+              <Link to="/" className="nav-link">À Propos</Link>
             </li>
           </ul>
-          {/* <ul className="navbar-nav mb-2 mb-lg-0">
+          <ul className="navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="signup" className="nav-link">Inscription</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="login" className="nav-link">Connexion</Link>
+              </li>
+          </ul>
+          {/* 
             {
               !user &&
-              <>
-                <li className="nav-item">
-                  <a to="register" className="nav-link">Inscription</a>
-                </li>
-                <li className="nav-item">
-                  <a to="login" className="nav-link">Connexion</a>
-                </li>
-              </>
+              
             }
             { 
               user && 
@@ -43,7 +45,7 @@ function Navbar() {
                 </li>
               </>
             }
-          </ul> */}
+           */}
         </div>
       </div>
     </nav>
