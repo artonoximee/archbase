10889a5059b9605faa4fd95704d25bca52cbaab1
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-11 11:37:56 +0200

    added proto dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { useAuth } from "../contexts/AuthContext";
 
 function Dashboard() {
+  const { currentUser } = useAuth();
+
   return(
-    <h1>Dashboard</h1>
+    <>
+      <div className="card mt-5 text-bg-dark">
+        <div className="card-header">
+          <h2 className="text-primary">Dashboard</h2>
+        </div>
+        <div className="card-body">
+          <p className="card-text">Conecté en tant que <b>{ currentUser.email }</b></p>
+        </div>
+      </div>
+    </>
   )
 }
 
