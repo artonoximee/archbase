d472585e94056ffb14931c1034964d3258b9fc64
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-11 11:15:51 +0200

    added react router and first routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,37 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css';
 
 import Navbar from "./Navbar";
+import Dashboard from "./Dashboard";
 import SignUp from "./auth/SignUp";
+import LogIn from "./auth/LogIn";
 import { AuthProvider } from "../contexts/AuthContext";
 
 function App() {
   return (
     <AuthProvider>
+      <Router>
       <div id="page" className="container-fluid bg-dark text-light">
         <div className="container">
           
           <div className="row justify-content-center">
             <div className="col-lg-8">
               <Navbar />
-              <SignUp />
+              
+                <Routes>
+                  <Route exact path="/" element={ <Dashboard /> } />
+                  <Route path="/signup" element={ <SignUp /> } />
+                  <Route path="/login" element={ <LogIn /> } />
+                </Routes>
+              
             </div>
           </div>
 
         </div>
       </div>
+      </Router>
     </AuthProvider>
   );
 }
--- src/components/auth/LogIn.js
+++ src/components/auth/LogIn.js
@@ -1,27 +1,8 @@
-import React, {useState} from "react";
-import {useForm} from "react-hook-form";
-import {signInWithEmailAndPassword} from "firebase/auth";
-import {auth} from '../../firebase-config'
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function SignIn() {
   const { register, handleSubmit, reset, formState: { errors } } = useForm();
-  const [errorLogin, setErrorLogin] = useState(false);
-  const navigate = useNavigate();
-
-  async function login(data) {
-    try {
-      await signInWithEmailAndPassword(auth, data.email, data.password);
-      setErrorLogin(false);
-      reset({
-        email: '',
-        password: ''
-      });
-      navigate('/')
-    } catch(err) {
-      setErrorLogin(true)
-    }
-  }
 
   return (
     <div className="row justify-content-center top-margin">
@@ -33,28 +14,28 @@ function SignIn() {
           <input 
             type="email"
             id="email"
-            className={`form-control form-control-lg bg-dark border-secondary text-light ${errors.email && "is-invalid border-danger"}`}
+            className={ `form-control form-control-lg bg-dark border-secondary text-light ${ errors.email && "is-invalid border-danger" }` }
             placeholder="Adresse email"
-            {...register("email", { required: true, pattern: /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i })}
+            { ...register("email", { required: true, pattern: /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i }) }
           />
-          {errors.email && <div className="form-text text-danger">Merci de renseigner votre adresse email pour vous connecter</div>}
+          { errors.email && <div className="form-text text-danger">Merci de renseigner votre adresse email pour vous connecter</div> }
 
           <label htmlFor="email" className="form-label mt-3"><i className="fa-sharp fa-solid fa-key text-primary"></i> Mot de passe</label>
           <input 
             type="password"
             id="password"
-            className={`form-control form-control-lg bg-dark border-secondary text-light ${errors.password && "is-invalid border-danger"}`}
+            className={ `form-control form-control-lg bg-dark border-secondary text-light ${ errors.password && "is-invalid border-danger" }` }
             placeholder="Mot de passe"
-            {...register("password", { required: true, minLength: 6 })}
+            { ...register("password", { required: true, minLength: 6 }) }
           />
-          <div class={`form-text ${errors.password && "text-danger"}`}>
+          <div class={ `form-text ${ errors.password && "text-danger" }` }>
             Votre mot de passe doit au minimum contenir 6 caractères.
           </div>
         
           <div className="d-grid gap-2">
-            <button className={`btn btn-lg mt-5 btn-outline-primary`} onClick={handleSubmit(login)} type="submit">Connexion</button>
+            <button className={ `btn btn-lg mt-5 btn-outline-primary` } onClick={ handleSubmit() } type="submit">Connexion</button>
           </div>
-          {errorLogin && <p className="text-danger text-center mt-2">Adresse email ou mot de passe invalide</p>}
+          {/* {errorLogin && <p className="text-danger text-center mt-2">Adresse email ou mot de passe invalide</p>} */}
 
           <div className=" bottom-margin"></div>
         </div>
